df0ee6d9c10c015aa95f315207b3f2025d5a3d25
martins.kevin@gmail.com <martins.kevin@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-08 02:54:06 +0000

    add some X angle support and check for "on edge"

--- trunk/play/VisionRobot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousCommand.java
+++ trunk/play/VisionRobot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousCommand.java
@@ -1,6 +1,7 @@
 
 package edu.wpi.first.wpilibj.templates.commands;
 
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.templates.MotorControlAssembly;
 import edu.wpi.first.wpilibj.templates.RobotMap;
 import edu.wpi.first.wpilibj.templates.RobotTemplate;
@@ -14,12 +15,14 @@ public class AutonomousCommand extends CommandBase {
 
     VisionSubsystem vision_;
     MotorControlAssembly tiltAssembly_;
+//    RobotDrive drive_;
     
     public AutonomousCommand() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         vision_ = RobotTemplate.visionSubsystem; 
-        tiltAssembly_ = RobotMap.tiltAssembly_; 
+        tiltAssembly_ = RobotMap.tiltAssembly; 
+//        drive_ = RobotMap.driveMechanumDrive;
     }
 
     // Called just before this Command runs the first time
@@ -29,18 +32,31 @@ public class AutonomousCommand extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        
-            if(vision_.goalFound())
-            {
+            if(vision_.goalFound() != VisionSubsystem.GOAL__NONE) {
+                switch(vision_.goalFound())
+                {
+                    case VisionSubsystem.GOAL__HIGH:
+                        System.out.println("Found High Goal");
+                        break;
+                    case VisionSubsystem.GOAL__MIDDLE:
+                        System.out.println("Found Middle Goal");
+                        break;
+                    case VisionSubsystem.GOAL__LOW:
+                        System.out.println("Found Low Goal");
+                        break;
+                }
+
                 //System.out.println("angle = "+tiltAssembly_.getAngle()+", vision = "+vision_.getYError());
-                tiltAssembly_.runFromInput(vision_.getYError()/34.0);
-                
+                tiltAssembly_.runFromInput(vision_.getYErrorNorm()*.8);
+//                drive_.mecanumDrive_Polar(0.0, 0.0, vision_.getXError()/28.0);
+                RobotMap.driveFLMotor.set(vision_.getXErrorNorm()*.8);
             }
             else
             {
                 tiltAssembly_.runFromInput(0);
-            }
-    
+//                drive_.mecanumDrive_Polar(0.0, 0.0, vision_.getXError()/28.0);
+                RobotMap.driveFLMotor.set(0.0);
+            }    
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -50,8 +66,8 @@ public class AutonomousCommand extends CommandBase {
 
     // Called once after isFinished returns true
     protected void end() {
-         vision_.disable();
-    
+        System.out.println("Autonomous Command ended!!!!!!!! Disabling Vision!!!!");
+         vision_.disable();    
     }
 
     // Called when another command which requires one or more of the same
