f721217b0200b77f20117bf22aa5129568f3abf8
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-13 00:51:49 +0000

    Inital Drive & Control

--- trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/OI.java
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/OI.java
@@ -1,44 +1,118 @@
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.DigitalIOButton;
+import edu.wpi.first.wpilibj.templates.LogitechController;
+import edu.wpi.first.wpilibj.templates.LogitechControllerButton;
+import edu.wpi.first.wpilibj.templates.commands.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+
+    //// CREATING BUTTONS
+    public static final int kLogitechFirstPort = 1;
+    private LogitechController m_FirstGamePad;
+    // Trigger Buttons (at index fingers)
+    private LogitechControllerButton m_setFirst_LeftShoulderButton;
+    private LogitechControllerButton m_setFirst_LeftTrigger;
+    private LogitechControllerButton m_setFirst_RightShoulderButton;
+    private LogitechControllerButton m_setFirst_RightTrigger;
+    // Gamepad
+    private LogitechControllerButton m_setFirst_GamepadLeft;
+    private LogitechControllerButton m_setFirst_GamepadRight;
+    private LogitechControllerButton m_setFirst_GamepadUp;
+    private LogitechControllerButton m_setFirst_GamepadDown;
+    // X,Y,A,B Buttons
+    private LogitechControllerButton m_setFirst_X;
+    private LogitechControllerButton m_setFirst_Y;
+    private LogitechControllerButton m_setFirst_A;
+    private LogitechControllerButton m_setFirst_B;
+    // Start/Back Buttoins
+    private LogitechControllerButton m_setFirst_Start;
+    private LogitechControllerButton m_setFirst_Back;
+    // Stick Buttons (Push down on stick)
+    private LogitechControllerButton m_setFirst_LeftStickButton;
+    private LogitechControllerButton m_setFirst_RightStickButton;
+
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
     // number it is.
     // Joystick stick = new Joystick(port);
     // Button button = new JoystickButton(stick, buttonNumber);
-    
     // Another type of button you can create is a DigitalIOButton, which is
     // a button or switch hooked up to the cypress module. These are useful if
     // you want to build a customized operator interface.
     // Button button = new DigitalIOButton(1);
-    
     // There are a few additional built in buttons you can use. Additionally,
     // by subclassing Button you can create custom triggers and bind those to
     // commands the same as any other Button.
-    
     //// TRIGGERING COMMANDS WITH BUTTONS
     // Once you have a button, it's trivial to bind it to a button in one of
     // three ways:
-    
     // Start the command when the button is pressed and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenPressed(new ExampleCommand());
-    
     // Run the command while the button is being held down and interrupt it once
     // the button is released.
     // button.whileHeld(new ExampleCommand());
-    
     // Start the command when the button is released  and let it run the command
     // until it is finished as determined by it's isFinished method.
     // button.whenReleased(new ExampleCommand());
-}
+    public OI() {
 
+        // Create the Controller
+        m_FirstGamePad = new LogitechController(kLogitechFirstPort);
+        // Create the Buttons
+        // Trigger Buttons (at index fingers)
+        m_setFirst_LeftShoulderButton = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonLeftB.value);
+        m_setFirst_LeftTrigger = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonLeftTrigger.value);
+        m_setFirst_RightShoulderButton = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonRightB.value);
+        m_setFirst_RightTrigger = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonRightTrigger.value);
+        // Gamepad
+        m_setFirst_GamepadLeft = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonGameLeft.value);
+        m_setFirst_GamepadRight = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonGameRight.value);
+        m_setFirst_GamepadUp = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonGameUp.value);
+        m_setFirst_GamepadDown = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonGameDown.value);
+        // X,Y,A,B Buttons
+        m_setFirst_X = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonX.value);
+        m_setFirst_Y = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonY.value);
+        m_setFirst_A = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonA.value);
+        m_setFirst_B = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonB.value);
+        // Start/Back Buttoins
+        m_setFirst_Start = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonStart.value);
+        m_setFirst_Back = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonBack.value);
+        // Stick Buttons (Push down on stick)
+        m_setFirst_LeftStickButton = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonLeftTop.value);
+        m_setFirst_RightStickButton = new LogitechControllerButton(m_FirstGamePad, LogitechController.ButtonType.kButtonRightTop.value);
+
+        // Associate the Buttons with Commands
+        m_setFirst_LeftShoulderButton.whenPressed(new PrintMessage("Button <m_setFirst_LeftShoulderButton> pressed\n"));
+        m_setFirst_LeftTrigger.whenPressed(new PrintMessage("Button <m_setFirst_LeftTrigger> pressed\n"));
+        m_setFirst_RightShoulderButton.whenPressed(new PrintMessage("Button <m_setFirst_RightShoulderButton> pressed\n"));
+        m_setFirst_RightTrigger.whenPressed(new PrintMessage("Button <m_setFirst_RightTrigger> pressed\n"));
+        // Gamepad
+        m_setFirst_GamepadLeft.whenPressed(new PrintMessage("Button <m_setFirst_GamepadLeft> pressed\n"));
+        m_setFirst_GamepadRight.whenPressed(new PrintMessage("Button <m_setFirst_GamepadRight> pressed\n"));
+        m_setFirst_GamepadUp.whenPressed(new PrintMessage("Button <m_setFirst_GamepadUp> pressed\n"));
+        m_setFirst_GamepadDown.whenPressed(new PrintMessage("Button <m_setFirst_GamepadDown> pressed\n"));
+        // X,Y,A,B Buttons
+        m_setFirst_X.whenPressed(new PrintMessage("Button <m_setFirst_X> pressed\n"));
+        m_setFirst_Y.whenPressed(new PrintMessage("Button <m_setFirst_Y> pressed\n"));
+        m_setFirst_A.whenPressed(new PrintMessage("Button <m_setFirst_A> pressed\n"));
+        m_setFirst_B.whenPressed(new PrintMessage("Button <m_setFirst_B> pressed\n"));
+        // Start/Back Buttoins
+        m_setFirst_Start.whenPressed(new PrintMessage("Button <m_setFirst_Start> pressed\n"));
+        m_setFirst_Back.whenPressed(new PrintMessage("Button <m_setFirst_Back> pressed\n"));
+        // Stick Buttons (Push down on stick)
+        m_setFirst_LeftStickButton.whenPressed(new PrintMessage("Button <m_setFirst_LeftStickButton> pressed\n"));
+        m_setFirst_RightStickButton.whenPressed(new PrintMessage("Button <m_setFirst_RightStickButton> pressed\n"));
+    }
+
+    public LogitechController getFirstController() {
+        return m_FirstGamePad;
+    }
+}
---
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.templates.AztechRobot;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
+/**
+ *
+ * @author MattKahn
+ */
+public class ManualDrive extends Command {
+
+    public ManualDrive() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(AztechRobot.drive);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        double rotation = -AztechRobot.oi.getFirstController().getRightY();
+        double y = -AztechRobot.oi.getFirstController().getRightX();
+        double x = -AztechRobot.oi.getFirstController().getTriggers();
+        double gyroAngle = 0;
+
+//        RobotMap.driveMechanumDrive.mecanumDrive_Cartesian( rotation, 0, 0, 0);
+        RobotMap.driveMechanumDrive.mecanumDrive_Cartesian(x, y, rotation, gyroAngle);
+        System.out.println("X= " + x + " Y= " + y + " 0= " + rotation);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
