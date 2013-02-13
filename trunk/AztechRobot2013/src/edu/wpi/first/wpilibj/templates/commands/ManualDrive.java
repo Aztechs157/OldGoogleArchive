add43f9ef4dd266e1aaa6bb6c561636f37bbac57
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-13 03:00:28 +0000

    Speed Control Drive works (but is disabled 'till the encoder wire gets fixed)

--- trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.templates.AztechRobot;
 import edu.wpi.first.wpilibj.templates.RobotMap;
@@ -33,6 +34,15 @@ public class ManualDrive extends Command {
 //        RobotMap.driveMechanumDrive.mecanumDrive_Cartesian( rotation, 0, 0, 0);
         RobotMap.driveMechanumDrive.mecanumDrive_Cartesian(x, y, rotation, gyroAngle);
         System.out.println("X= " + x + " Y= " + y + " 0= " + rotation);
+                try {
+            System.out.print("FR.getX()=" + RobotMap.driveDriveFR.getX());
+            System.out.print(" FL.getX()=" + RobotMap.driveDriveFL.getX());
+            System.out.print(" RR.getX()=" + RobotMap.driveDriveRR.getX());
+            System.out.println(" RL.getX()=" + RobotMap.driveDriveRL.getX());
+        } catch (CANTimeoutException ex) {
+            // don't care
+        }
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
