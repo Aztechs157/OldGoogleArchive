6cf6ebbf8ae9de5ad03137be52f61791d0d694b1
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-15 00:03:45 +0000

    Initial framework for shooter

--- AztechRobot2013/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ AztechRobot2013/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -66,7 +66,9 @@ public class RobotMap {
         
     public static void init() {
 
-
+        AztechRobot.shooter.init();
+        
+        
         try {
             shooterElevation = new CANJaguar(ShooterElevationMotorID);
             shooterElevation.changeControlMode(CANJaguar.ControlMode.kPosition);
