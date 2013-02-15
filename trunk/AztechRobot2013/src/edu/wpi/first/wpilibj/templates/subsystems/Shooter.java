7f0a461da4ce4b34a8be8a10545dfde0f342031e
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-15 01:32:55 +0000

    Shooter Code

--- trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
@@ -4,7 +4,7 @@
  */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
-import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.templates.RobotMap;
@@ -17,10 +17,27 @@ public class Shooter extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
     public static CANJaguar shooterElevation;
+    public static Talon shooterFirstStage;   // PWM
+    public static Talon shooterSecondStage;
+    
+    // Pneumatics
+    public static Compressor compressor;
 
+    Solenoid loaderRetract;
+    Solenoid loaderDeploy;
+    Solenoid shooterRetract;
+    Solenoid shooterDeploy;
+            
     public void init()
     {
-                
+        compressor = new Compressor(RobotMap.PressureSwitchGPIOPort, RobotMap.CompressorRelayChannel);
+        compressor.start();
+
+        loaderRetract = new Solenoid(RobotMap.LoaderRetractPort);
+        loaderDeploy = new Solenoid(RobotMap.LoaderDeployPort);
+        shooterRetract = new Solenoid(RobotMap.ShooterRetractPort);
+        shooterDeploy = new Solenoid(RobotMap.ShooterDeployPort);
+         
         try {
             shooterElevation = new CANJaguar(RobotMap.ShooterElevationMotorID);
             shooterElevation.changeControlMode(CANJaguar.ControlMode.kPosition);
@@ -33,7 +50,8 @@ public class Shooter extends Subsystem {
             ex.printStackTrace();
         }
 
-
+        shooterFirstStage = new Talon(RobotMap.ShooterFirstStageDrivePWMPort);
+        shooterSecondStage = new Talon(RobotMap.ShooterSecondStageDrivePWMPort);
     }
     
     public void initDefaultCommand() {
@@ -41,5 +59,89 @@ public class Shooter extends Subsystem {
         //setDefaultCommand(new MySpecialCommand());
     }
     
+    public void spinLaunchWheels(double power)
+    {
+        shooterFirstStage.set(power);
+        shooterSecondStage.set(power);
+    }
     
+    public void stopLaunchWheels()
+    {
+        spinLaunchWheels(0);
+    }
+   
+    public void extendLoader()
+    {
+        setLoaderExtended(true);
+    }
+    public void retractLoader()
+    {
+        setLoaderExtended(false);        
+    }
+    public void extendShooter()
+    {
+        setShooterExtended(true);
+    }
+    public void retractShooter()
+    {
+        setShooterExtended(false);        
+    }
+    
+    private void setLoaderExtended(boolean extend) {
+        if (extend) {
+            if ((loaderRetract != null) && (loaderDeploy != null)) {
+                loaderDeploy.set(true);
+                loaderRetract.set(false);
+            } else {
+                if ((loaderRetract != null) && (loaderDeploy != null)) {
+                    loaderRetract.set(true);
+                    loaderDeploy.set(false);
+                }
+            }
+        }
+    }
+
+    public void adjustShooterElevation(double delta)
+    {
+        setShooterElevation(getShooterElevation() + delta);
+    }
+    
+    public void setShooterElevation(double elevation)
+    {
+        if (null != shooterElevation) {
+            try {
+                shooterElevation.setX(elevation);
+            } catch (edu.wpi.first.wpilibj.can.CANTimeoutException e) {
+                System.out.println("CAN Timeout setting shooter elevation (motor " + RobotMap.ShooterElevationMotorID + ")");
+            }
+        }
+    }
+    
+    private static double lastValidResult = 0;
+    public double getShooterElevation()
+    {
+       double result = lastValidResult;
+       if (null != shooterElevation) {
+            try {
+                result = shooterElevation.getX();
+            } catch (edu.wpi.first.wpilibj.can.CANTimeoutException e) {
+                System.out.println("CAN Timeout reading shooter elevation (motor " + RobotMap.ShooterElevationMotorID + ")");
+            }
+        }
+       return result;
+    }
+    
+    private void setShooterExtended(boolean extend) {
+        if (extend) {
+            if ((shooterRetract != null) && (shooterDeploy != null)) {
+                shooterDeploy.set(true);
+                shooterRetract.set(false);
+            } else {
+                if ((shooterRetract != null) && (shooterDeploy != null)) {
+                    shooterRetract.set(true);
+                    shooterDeploy.set(false);
+                }
+            }
+        }
+    }
 }
