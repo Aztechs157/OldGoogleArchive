2b5a4aafc8bbbd2bbc15e6cc425d4efa31f4fb1d
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-16 19:14:33 +0000

    Manual Aim updates

--- AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/Turn.java
+++ AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/Turn.java
@@ -4,50 +4,58 @@
  */
 package edu.wpi.first.wpilibj.templates.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.templates.AztechRobot;
-import edu.wpi.first.wpilibj.templates.subsystems.Shooter;
 
 /**
  *
  * @author MattKahn
  */
-public class ManualAim extends CommandBase {
+public class Turn extends CommandBase {
     
-    public ManualAim() {
+    double degrees = 0;
+    double startTime;
+    double duration = 0;
+    
+    public Turn(double inDegrees) {
         // Use requires() here to declare subsystem dependencies
-        requires(AztechRobot.shooter);
+        // eg. requires(chassis);
+        
+        degrees = inDegrees;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        System.out.println("MANUAL AIM ===================================================");
+        AztechRobot.drive.setCoastMode(false);
+        AztechRobot.drive.mecanumDrive_Cartesian(0, 0, degrees);
+
+        duration = .75;
+        startTime = Timer.getFPGATimestamp();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        double rotation;
-        double elevation;
- 
-        rotation = 0.2 * oi.getOperatorJoystick().getX();
-        elevation = oi.getOperatorJoystick().getY();
-
-//        AztechRobot.drive.mecanumDrive_Cartesian(0, 0, rotation);
-//        AztechRobot.shooter.adjustShooterElevation(elevation);
-        System.out.println("Elevation = " + AztechRobot.shooter.getShooterElevation() + " + " + elevation + "  -- Az  += " + rotation);
+        if (startTime >= (Timer.getFPGATimestamp() + duration)) {
+            AztechRobot.drive.mecanumDrive_Cartesian(0, 0, 0);
+        }
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        if (startTime >= (Timer.getFPGATimestamp() + duration + 0.2)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
     // Called once after isFinished returns true
     protected void end() {
+        AztechRobot.drive.setCoastMode(true);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
