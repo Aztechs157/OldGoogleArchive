e17f99977b90329d8dacd0ba0189ed610b976b87
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-02-19 21:10:24 +0000

    Change vision alignment to use turn command/set elevation commands

--- trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
@@ -21,7 +21,7 @@ public class ManualDrive extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        System.out.println("Initializing ManualDrive");
+        System.out.println("Starting ManualDrive");
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -91,10 +91,12 @@ public class ManualDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+        System.out.println("Stopping Manual Drive");
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        end();
     }
 }
