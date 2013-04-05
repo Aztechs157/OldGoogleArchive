f3acaa91923beb0a040a536f16251d933d657e9a
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2013-04-05 01:51:25 +0000

    Competition Calibration (side mounted pot)

--- AztechRobot2013/src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
+++ AztechRobot2013/src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
@@ -26,15 +26,15 @@ public class Shooter extends Subsystem {
     public static final double highLimitAngle = 50;
     public static final double loadingAngle = 20;
     public static final double climbAngle = 0;
-    public static final double basicShotAngle = 40;
+    public static final double basicShotAngle = 42;
     //             Sensor   Angle
     // Bottom Stop 0.856    0.00
     // Top Stop    0.652    50.9
     // Sensor values per degree .004 
-    public static final double zeroSensorReading = 0.360;
-    public static final double maxSensorReading = 0.597;
+    public static final double zeroSensorReading = .065;
+    public static final double maxSensorReading =  .276;
     public static final double zeroElevationAngle = 0;
-    public static final double maxElevationAngle = 53.6;
+    public static final double maxElevationAngle = 50;
     public static final double sensorPerDegree = ((maxSensorReading - zeroSensorReading) / (maxElevationAngle - zeroElevationAngle));
     // Up PID Constants
     public static final double upP = 5000.0;
