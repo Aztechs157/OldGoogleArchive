e6154bff96c6cf03d22fa622eca0f7bb5ed35305
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2014-01-27 01:08:12 +0000

    Initial version

---
+++ AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/ScaledCANJaguar.java
@@ -0,0 +1,83 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc157.AztechRobot2014;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
+
+/**
+ *
+ * @author MattKahn
+ */
+public class ScaledCANJaguar extends CANJaguar {
+
+    private double scalingFactor = 1;
+
+    public ScaledCANJaguar(int deviceNumber) throws CANTimeoutException {
+        super(deviceNumber);
+    }
+
+    public ScaledCANJaguar(int deviceNumber, ControlMode controlMode) throws CANTimeoutException {
+        super(deviceNumber, controlMode);
+    }
+
+    public void setScalingFactor(double scale) {
+        scalingFactor = scale;
+    }
+
+    public void setX(double setPoint) throws CANTimeoutException {
+//        System.out.println("SX");
+        super.setX(setPoint * scalingFactor);
+    }
+
+    public void set(double setPoint) {
+//        System.out.println("S");
+        super.set(setPoint * scalingFactor);
+    }
+
+    public void set(double setPoint, byte b) {
+//        System.out.println("S..");
+        super.set(setPoint * scalingFactor, b);
+    }
+
+    public double get() {
+        return (super.get() / scalingFactor);
+    }
+
+    public double getX() throws CANTimeoutException {
+        return super.getX();// / scalingFactor;
+    }
+    ControlMode controlMode;
+
+    public void changeControlMode(ControlMode newControlMode) throws CANTimeoutException {
+        controlMode = newControlMode;
+        super.changeControlMode(controlMode);
+    }
+
+    public ControlMode getCommandedControlMode()
+    {
+        return controlMode;
+    }
+    
+    public boolean isInCommandedControlMode() {
+        ControlMode currentControlMode = CANJaguar.ControlMode.kCurrent;;
+        int tries = 0;
+        boolean failed = false;
+        do {
+            try {
+                currentControlMode = super.getControlMode();
+            } catch (CANTimeoutException ex) {
+                failed = true;
+            }
+        } while (failed && (tries++ < RobotMap.m_kMaxCANRetries));
+
+        if (tries >= RobotMap.m_kMaxCANRetries) {
+            return false;  // can't actually tell, so assume bad
+        }
+
+        return (currentControlMode == controlMode);
+//      return (controlMode
+    }
+}
