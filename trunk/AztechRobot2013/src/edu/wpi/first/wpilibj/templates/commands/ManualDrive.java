81be0c3d1541349205f59f7b3e9b237740345ac2
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2014-01-31 01:28:46 +0000

    Update to tank drive and non-mechanum wheels

--- trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
+++ trunk/AztechRobot2013/src/edu/wpi/first/wpilibj/templates/commands/ManualDrive.java
@@ -27,60 +27,64 @@ public class ManualDrive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
-        // Driver Control    
-        double x = -CommandBase.oi.getDriverController().getLeftX();
-        double y = -CommandBase.oi.getDriverController().getLeftY();
-        double rotation = -CommandBase.oi.getDriverController().getRightX();
-        double strafe = CommandBase.oi.getDriverController().getTriggers();
-        
-        double minSpeedLimit = 1.0;//0.75;//0.5;
-        double speedLimit = minSpeedLimit + 0;
-
-        // if the left stick is pressed down
-        if (CommandBase.oi.getDriverController().getButtonLeftTop())
-        {
-            speedLimit = 1.0;           // set top speed
-            x = 0.5 * x;                // reduce the strafeing rate
-            rotation = 0.5 * rotation;  // reduce the turning rate
-                                        // but leave button strafe alone
-        }
-        
-        if (speedLimit > 1.0) speedLimit = 1.0;        
-                
-        if(x*x < 0.01)
-        {
-            x = 0;
-        }
-        if(y*y < 0.01)
-        {
-            y = 0;
-        }
-        if(strafe*strafe < 0.1)
-        {
-            strafe = 0;
-        }
-        if(rotation*rotation < 0.01)
-        {
-            rotation = 0;
-        }
-        
-        x = strafe + speedLimit * (x * x * (x<0 ? -1.0 : 1.0));
-        y = speedLimit * (y * y * (y<0 ? -1.0 : 1.0));
-        rotation = speedLimit * (rotation * rotation * (rotation<0 ? -1.0 : 1.0));
-
-        // Operator Control
-        double operatorSpeed = 0.4;
-        double operatorRotation = CommandBase.oi.getOperatorJoystick().getX();
-        double operatorForeAft = CommandBase.oi.getOperatorJoystick().getY();
-        
-        if((operatorRotation * operatorRotation) < 0.025) operatorRotation = 0;
-        double operatorRotationSpeed =  operatorSpeed * operatorRotation;
-
-        if((operatorForeAft * operatorForeAft) < 0.025) operatorForeAft = 0;
-        double operatorForeAftSpeed = operatorSpeed * operatorForeAft;
-        
-        
-        CommandBase.drive.mecanumDrive_Cartesian(x, y + operatorForeAftSpeed, rotation + operatorRotationSpeed);
+//        // Driver Control    
+//        double x = -CommandBase.oi.getDriverController().getLeftX();
+//        double y = -CommandBase.oi.getDriverController().getLeftY();
+//        double rotation = -CommandBase.oi.getDriverController().getRightX();
+//        double strafe = CommandBase.oi.getDriverController().getTriggers();
+//        
+//        double minSpeedLimit = 1.0;//0.75;//0.5;
+//        double speedLimit = minSpeedLimit + 0;
+//
+//        // if the left stick is pressed down
+//        if (CommandBase.oi.getDriverController().getButtonLeftTop())
+//        {
+//            speedLimit = 1.0;           // set top speed
+//            x = 0.5 * x;                // reduce the strafeing rate
+//            rotation = 0.5 * rotation;  // reduce the turning rate
+//                                        // but leave button strafe alone
+//        }
+//        
+//        if (speedLimit > 1.0) speedLimit = 1.0;        
+//                
+//        if(x*x < 0.01)
+//        {
+//            x = 0;
+//        }
+//        if(y*y < 0.01)
+//        {
+//            y = 0;
+//        }
+//        if(strafe*strafe < 0.1)
+//        {
+//            strafe = 0;
+//        }
+//        if(rotation*rotation < 0.01)
+//        {
+//            rotation = 0;
+//        }
+//        
+//        x = strafe + speedLimit * (x * x * (x<0 ? -1.0 : 1.0));
+//        y = speedLimit * (y * y * (y<0 ? -1.0 : 1.0));
+//        rotation = speedLimit * (rotation * rotation * (rotation<0 ? -1.0 : 1.0));
+//
+//        // Operator Control
+//        double operatorSpeed = 0.4;
+//        double operatorRotation = CommandBase.oi.getOperatorJoystick().getX();
+//        double operatorForeAft = CommandBase.oi.getOperatorJoystick().getY();
+//        
+//        if((operatorRotation * operatorRotation) < 0.025) operatorRotation = 0;
+//        double operatorRotationSpeed =  operatorSpeed * operatorRotation;
+//
+//        if((operatorForeAft * operatorForeAft) < 0.025) operatorForeAft = 0;
+//        double operatorForeAftSpeed = operatorSpeed * operatorForeAft;
+//        
+        double left = CommandBase.oi.getDriverController().getLeftY();
+        double right = CommandBase.oi.getDriverController().getRightY();
+        double scLeft = (left * left) * (left < 0 ? -1 : 1);
+        double scRight = (right * right) * (right < 0 ? -1 : 1);
+        CommandBase.drive.tankDrive(-scLeft, scRight);
+//        CommandBase.drive.mecanumDrive_Cartesian(x, y + operatorForeAftSpeed, rotation + operatorRotationSpeed);
 
 //        System.out.println("x="+x+",    y="+y+",    r="+rotation+",    or="+operatorRotationSpeed);
 //                try {
