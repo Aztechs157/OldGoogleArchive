7ce255264bb91e662594480a57120ba5e74d0cf2
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2014-02-12 00:08:00 +0000

    Timeouts on sensor commands

--- AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/commands/DriveToRange.java
+++ AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/commands/DriveToRange.java
@@ -5,6 +5,7 @@
  */
 package org.usfirst.frc157.AztechRobot2014.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc157.AztechRobot2014.Robot;
 
@@ -18,6 +19,9 @@ public class DriveToRange extends Command {
 
     private static final double RangeTolerance = 10; //cm
 
+    private double stopTime;
+    private final double maxAlignmentTime = 2.0;
+
     public DriveToRange(double _desiredRange) {
         // Use requires() here to declare subsystem dependencies
         requires(Robot.drive);
@@ -28,6 +32,7 @@ public class DriveToRange extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        stopTime = Timer.getFPGATimestamp() + maxAlignmentTime;
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -35,19 +40,21 @@ public class DriveToRange extends Command {
         double range = getRangeToWall();
         double drive = (1.1 * range) / Robot.sensor.getUltrasonicSensor1().getMaxRange();
         Robot.drive.tankDrive(range, range);
-                
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         if (Math.abs(getRangeToWall() - desiredRange) < RangeTolerance) {
             return true;
+        } else if (Timer.getFPGATimestamp() > stopTime) {
+            return true;
         } else {
             return false;
         }
     }
 
-    // Called once after isFinished returns true
+// Called once after isFinished returns true
     protected void end() {
         Robot.drive.tankDrive(0, 0);
     }
