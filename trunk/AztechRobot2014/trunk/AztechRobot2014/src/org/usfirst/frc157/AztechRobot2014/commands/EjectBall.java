1a4db94405c576ef449a3fcb7fb02e826659d1c4
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2014-02-17 02:59:11 +0000

    New ingester commands (combined wheels and arm)

---
+++ trunk/AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/commands/EjectBall.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc157.AztechRobot2014.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc157.AztechRobot2014.subsystems.BallRetriever;
+
+/**
+ *
+ * @author mattkahn
+ */
+public class EjectBall extends CommandGroup {
+    
+    public EjectBall() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        addSequential(new MoveBallRetriever(BallRetriever.Eject));  // move to 3m from the wall
+        addSequential(new SpinRoller(SpinRoller.ROLLER_OUT));
+    } 
+}
