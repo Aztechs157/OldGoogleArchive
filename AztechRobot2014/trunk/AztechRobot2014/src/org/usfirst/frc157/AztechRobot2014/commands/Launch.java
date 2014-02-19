7e745110fa38139b4c5913b78776e5b5113a1f85
makahn@gmail.com <makahn@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2014-02-19 00:12:01 +0000

    Fix finished criteria

--- AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/commands/Launch.java
+++ AztechRobot2014/trunk/AztechRobot2014/src/org/usfirst/frc157/AztechRobot2014/commands/Launch.java
@@ -59,7 +59,7 @@ public class Launch extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return readyToLaunch;
+        return finshed;
     }
 
     // Called once after isFinished returns true
