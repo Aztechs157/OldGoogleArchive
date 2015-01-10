95cf90f36a881c73111bbb2d400943a1a7cd2a91
nareddyt <tejunareddy@gmail.com>
2015-01-10 19:47:35 +0000

    2015 Starting Code Has 4 drive jaguars using two joysticks (one for each side)

---
+++ Aztechs Robot 2015/src/org/usfirst/frc/team157/robot/ScaledCANJaguar.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc.team157.robot;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+
+/**
+ *
+ * @author MattKahn
+ */
+public class ScaledCANJaguar extends CANJaguar
+{
+	
+	private double scalingFactor = 1;
+	
+	public ScaledCANJaguar(int deviceNumber)
+	{
+		super(deviceNumber);
+	}
+	
+	public void setScalingFactor(double scale)
+	{
+		scalingFactor = scale;
+		System.out.println("Scaling factor: " + scale);
+	}
+	
+	public double getScalingFactor()
+	{
+		return scalingFactor;
+	}
+	
+	@Override
+	public void set(double setPoint)
+	{
+		// System.out.println("S");
+		super.set(setPoint * scalingFactor);
+	}
+	
+	@Override
+	public void set(double setPoint, byte b)
+	{
+		// System.out.println("S..");
+		super.set(setPoint * scalingFactor, b);
+	}
+	
+	@Override
+	public double get()
+	{
+		return (super.get() / scalingFactor);
+	}
+}
