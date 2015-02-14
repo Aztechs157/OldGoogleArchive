fda26a398dbb4ae587e3ccef45a98e9520a47689
john.n.vedder@gmail.com <john.n.vedder@gmail.com@1fd8a349-12b6-bb36-700f-8cce1990c19d>
2015-02-14 02:54:53 +0000

    Update Forks to Simple Manual Control on Driver's Left/Right Triggers

---
+++ trunk/AztechsRobot2015/src/org/usfirst/frc/team157/robot/commands/SimpleManualControlDown.java
@@ -0,0 +1,60 @@
+
+package org.usfirst.frc.team157.robot.commands;
+
+import org.usfirst.frc.team157.robot.subsystems.ForkliftPart;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SimpleManualControlDown extends Command
+{
+	
+	private ForkliftPart part;
+	
+	public SimpleManualControlDown(ForkliftPart part)
+	{
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(part);
+		this.part = part;
+	}
+	
+	// Called once after isFinished returns true
+	@Override
+	protected void end()
+	{
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute()
+	{
+		// if (part.getJagPosition() - part.getLowEndEncoderLimit() > 0.05)
+		
+		System.out.println("Setpoint = " + part.getJagSetpoint());
+		part.setJag(part.getLowEndEncoderLimit());
+		
+	}
+	
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize()
+	{
+		
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted()
+	{
+	}
+	
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished()
+	{
+		return true;
+	}
+}
---
+++ trunk/AztechsRobot2015/src/org/usfirst/frc/team157/robot/commands/SimpleManualControlUp.java
@@ -0,0 +1,60 @@
+
+package org.usfirst.frc.team157.robot.commands;
+
+import org.usfirst.frc.team157.robot.subsystems.ForkliftPart;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class SimpleManualControlUp extends Command
+{
+	
+	private ForkliftPart part;
+	
+	public SimpleManualControlUp(ForkliftPart part)
+	{
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(part);
+		this.part = part;
+	}
+	
+	// Called once after isFinished returns true
+	@Override
+	protected void end()
+	{
+	}
+	
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute()
+	{
+		// if (part.getHighEndEncoderLimit() - part.getJagPosition() > 0.05)
+		
+		System.out.println("Setpoint = " + part.getJagSetpoint());
+		part.setJag(part.getHighEndEncoderLimit());
+		
+	}
+	
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize()
+	{
+		
+	}
+	
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted()
+	{
+	}
+	
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished()
+	{
+		return true;
+	}
+}
